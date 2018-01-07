f4a9e15787134bef8cf1b93a57827ee4cc4cbb42
Tano Abeleyra <tano@abeleyra.com>
2018-01-07 00:03:59 -0300

    Add alert command

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ import threading
 
 from cryptomkt import Cryptomkt
 from datetime import datetime
-from telegram.ext import CommandHandler, Updater
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
-from models import session, Chat, Market
+from models import session, Alert, Chat, Market
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
@@ -55,9 +55,43 @@ def price(bot, update):
     update.message.reply_text(text, parse_mode=telegram.ParseMode.MARKDOWN)
 
 
+def add_alert(bot, update, price=None):
+    if price is None:
+        return update.message.reply_text("Ingrese el precio:")
+    try:
+        price = int(price)
+    except ValueError:
+        return update.message.reply_text("El precio debe ser un número entero.")
+    if price <= 0:
+        return update.message.reply_text("El precio debe ser un número mayor a 0.")
+    market = session.query(Market).filter_by(code=market_code).first()
+    trigger_on_lower = price <= market.price
+    alert_data = {
+        'chat_id': update.message.chat.id,
+        'price': price,
+        'trigger_on_lower': trigger_on_lower
+    }
+    alert = Alert(**alert_data)
+    session.add(alert)
+    session.commit()
+    sign = 'menor' if trigger_on_lower else 'mayor'
+    text = "Perfecto, te enviaré una alerta cuando el precio sea _{}_ a *${}*.".format(sign, price)
+    update.message.reply_text(text, parse_mode=telegram.ParseMode.MARKDOWN)
+
+
+def text_handler(bot, update):
+    words = update.message.text.split()
+    if len(words) != 1:
+        text = "Lo siento, no te entiendo. ¿Necesitas /ayuda?"
+        return update.message.reply_text(text)
+    return add_alert(bot, update, words[0])
+
+
 update_price()
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("precio", price))
+dispatcher.add_handler(CommandHandler("alerta", add_alert))
+dispatcher.add_handler(MessageHandler(Filters.text, text_handler))
 updater.start_webhook(listen='0.0.0.0',
                       port=8443,
                       url_path=BOT_TOKEN,
